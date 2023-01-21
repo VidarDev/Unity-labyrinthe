df81d4bca61023c0b1aecc188d862b7100912bc9
Fanny_goup <goupfanny@gmail.com>
2023-01-21 21:56:47 +0100

    fix: ajout du script playerCollider sur l'objet sphere

--- Marble_Race/Assets/Script/playerCollider.cs
+++ Marble_Race/Assets/Script/playerCollider.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerCollider : MonoBehaviour
 {
@@ -19,7 +20,7 @@ public class playerCollider : MonoBehaviour
     {
         if(col.gameObject.tag.Equals("winObject"))
         {
-            Application.LoadLevel("WinScene");
+            SceneManager.LoadScene("winScene");
         }
     }
 
